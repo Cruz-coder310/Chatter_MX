53bdf9a26915c9e9928d25dd4c371bd22a2f7b53
jcarcamo <carcamo1cruz@gmail.com>
2026-01-13 21:31:48 -0600

    feat(accounts): add support for password update

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,6 +9,16 @@ urlpatterns = [
     path("accounts/login/", auth_view.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_view.LogoutView.as_view(), name="logout"),
     path("accounts/register/", views.registration, name="registration"),
+    path(
+        "accounts/password-change/",
+        auth_view.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "accounts/password-change/done/",
+        auth_view.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     # Temporary home page for testing LOGOUT_REDIRECT_URL
     path("", views.home, name="home"),
 ]
