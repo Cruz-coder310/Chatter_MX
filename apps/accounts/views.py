651c7b46cb423f075f89ce15027296ecbdc119c8
jcarcamo <carcamo1cruz@gmail.com>
2026-01-23 22:59:07 -0600

    feat(accounts): add django messages framework

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import CustomUserCreationForm, UserEditForm
 
@@ -18,6 +19,7 @@ def registration(request):
         form = CustomUserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "You have successfully registered")
             return redirect("home")
 
     else:
@@ -36,6 +38,8 @@ def edit_profile(request):
         )
         if form.is_valid():
             form.save()
+            messages.success(request, "Your profile has been successfully edited")
+            return redirect("edit_profile")
     else:
         form = UserEditForm(instance=request.user)
 
