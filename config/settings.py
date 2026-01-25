71a9d79137568f97b52c8a6f0515feb4c0c6fb8b
jcarcamo <carcamo1cruz@gmail.com>
2026-01-24 23:05:06 -0600

    feat(accounts): add custom authentication backend for email login

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "apps.accounts.authentication.EmailAuthBackend",
+]
